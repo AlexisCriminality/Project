69cfad97c9b05f3538c10f1b3097913e16988c2a
79116 <alexsilver1994@gmail.com>
2023-02-14 00:22:02 +0300

    blank line added, testing Git

--- lesson.py
+++ lesson.py
@@ -31,6 +31,7 @@ def pendulum_stop(ampl_start, ampl_finish):
         ampl_start -= ampl_start * 0.084
     return count
 
+
 ampl_start = float(input('Введите начальную амплитуду: '))
 ampl_finish = float(input('Введите амплитуду остановки: '))
 
